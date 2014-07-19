6a1aff11f6cceed1b973614427e408cb9c3efbe2
Julien Deniau <julien.deniau@gmail.com>
2014-07-19 18:04:22 +0200

    Cookies and index page

--- web/assets/js/blendwebmix.js
+++ web/assets/js/blendwebmix.js
@@ -1,13 +1,36 @@
-Reveal.addEventListener(
-    'newsletter',
-    function () {
-        window.setTimeout(
-            function () {
-                $.albox({
-                    url: '/newsletter.html'
-                });
-        },
+Reveal.addEventListener('newsletter', function () {
+    window.setTimeout(
+        function () { $.albox({ url: '/newsletter.html' }); },
         5000
-        );
-    }
-);
+    );
+});
+
+Reveal.addEventListener('presentation-title', function () {
+    var timeout = 800;
+
+    $('.presentation-title .current-custom-fragment').removeClass('current-custom-fragment');
+    $('.presentation-title .custom-fragment').first().addClass('current-custom-fragment');
+
+    var nextFragment = function() {
+        var cur = $('.presentation-title .custom-fragment.current-custom-fragment');
+        var next = $(cur).next('.custom-fragment');
+
+        if ($(next).length > 0) {
+            $(cur).removeClass('current-custom-fragment');
+            $(next).addClass('current-custom-fragment');
+        }
+
+        timeout = timeout * 2 / 3;
+
+        return next;
+    };
+
+    callback = function () {
+        var next = nextFragment();
+        if ($(next).length > 0) {
+            window.setTimeout(callback, timeout);
+        }
+    };
+
+    window.setTimeout(callback, timeout);
+});
